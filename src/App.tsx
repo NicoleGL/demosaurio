d16bdc0e86374726f02fa36aed091920775520c0
NicoleGL <nicole.glanga@gmail.com>
2023-09-26 18:50:58 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './screens/Home';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className="main-div">
+        <Home />
+      </div>
+    </>
+  )
+}
+
+export default App
