0b393f565009dc098a51b4c67ed431f1308b4192
NicoleGL <nicole.glanga@gmail.com>
2023-10-01 23:38:25 +0200

    Organizar CSS

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import LoginCSS from './index.module.css';
+import CSS from './index.module.css';
 
 function LoginPage() {
 
 
     return(
-        <div className={LoginCSS.main}>
-            <div className={LoginCSS.square}>
-                <h1 className={LoginCSS.header}>Iniciar sesión</h1>
-                <form className={LoginCSS["form-box"]}>
+        <div className={CSS["main-div"]}>
+            <div className={CSS["square-box"]}>
+                <h1 className={`${CSS.header} ${CSS["side-margin"]}`}>Iniciar sesión</h1>
+                <form className={`${CSS["form-box"]} ${CSS["side-margin"]}`}>
                     <label for="username">Usuario:</label><br />
-                    <input type="user" id="username" name="username" className={LoginCSS["enter-text"]} /><br />
+                    <input type="user" id="username" name="username" className={CSS["enter-text"]} /><br />
                     <label for="password">Contraseña:</label><br />
-                    <input type="password" id="password" name="password" className={LoginCSS["enter-text"]}/><br />
-                    <input type="submit" id="submit" name="password" value="Iniciar sesión" className={LoginCSS["submit-button"]} />
+                    <input type="password" id="password" name="password" className={CSS["enter-text"]}/><br />
+                    <input type="submit" id="submit" name="password" value="Iniciar sesión" className={CSS["submit-button"]} />
                 </form> 
-                <p>¿No tienes cuenta? <a>Regístrate</a></p>
+                <p className={CSS["side-margin"]}>¿No tienes cuenta? <a>Regístrate</a></p>
             </div>
         </div>
     )
