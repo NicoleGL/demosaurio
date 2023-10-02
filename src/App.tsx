ce35abd522e2e99c967c7ad585ed5870c9c8cd79
NicoleGL <nicole.glanga@gmail.com>
2023-10-02 15:21:45 +0200

    Añadir router

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import Home from './screens/Home';
 import Login from './screens/Login';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
+    <Router>
       <div className="main-div">
-        <Login />
+        <Routes>
+            <Route exact path='/' element={<Home />}></Route>
+            <Route exact path='/login' element={<Login />}></Route>
+        </Routes>
       </div>
-    </>
+    </ Router>
   )
 }
 
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import CSS from './index.module.css';
 
 function LoginPage() {
+    
+    let username= "";
+    let password = "";
 
+    console.log(username);
 
     return(
         <div className={CSS["main-div"]}>
             <div className={CSS["square-box"]}>
                 <h1 className={`${CSS.header} ${CSS["side-margin"]}`}>Iniciar sesión</h1>
                 <form className={`${CSS["form-box"]} ${CSS["side-margin"]}`}>
-                    <label for="username">Usuario:</label><br />
-                    <input type="user" id="username" name="username" className={CSS["enter-text"]} /><br />
-                    <label for="password">Contraseña:</label><br />
-                    <input type="password" id="password" name="password" className={CSS["enter-text"]}/><br />
+                    <label htmlFor="username">Usuario:</label><br />
+                    <input type="user" id="username" name="username" value={username} className={CSS["enter-text"]} /><br />
+                    <label htmlFor="password">Contraseña:</label><br />
+                    <input type="password" id="password" name="password" value={password} className={CSS["enter-text"]}/><br />
                     <input type="submit" id="submit" name="password" value="Iniciar sesión" className={CSS["submit-button"]} />
                 </form> 
                 <p className={CSS["side-margin"]}>¿No tienes cuenta? <a>Regístrate</a></p>
