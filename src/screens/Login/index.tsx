011248e003ae42aefa6347e8b08d9e490d9449ff
PabloPI4 <pif2110@gmail.com>
2023-10-03 17:24:08 +0200

    Primera prueba. Imprime el usuario y contraseña puestos en login en el puerto 8080 (backend) al darle al boton de iniciar sesion

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import CSS from './index.module.css';
+import {send} from '../../App.tsx';
+import sendMsg from '../../main.tsx';
+
+function sendMessage() {
+    sendMsg(username.value + ", " + password.value)
+}
 
 function LoginPage() {
     
@@ -14,10 +20,10 @@ function LoginPage() {
                 <h1 className={`${CSS.header} ${CSS["side-margin"]}`}>Iniciar sesión</h1>
                 <form className={`${CSS["form-box"]} ${CSS["side-margin"]}`}>
                     <label htmlFor="username">Usuario:</label><br />
-                    <input type="user" id="username" name="username" value={username} className={CSS["enter-text"]} /><br />
+                    <input type="user" id="username" name="username" className={CSS["enter-text"]} /><br />
                     <label htmlFor="password">Contraseña:</label><br />
-                    <input type="password" id="password" name="password" value={password} className={CSS["enter-text"]}/><br />
-                    <input type="submit" id="submit" name="password" value="Iniciar sesión" className={CSS["submit-button"]} />
+                    <input type="password" id="password" name="password" className={CSS["enter-text"]}/><br />
+                    <input onClick = {sendMessage} type="submit" id="submit" name="password" className={CSS["submit-button"]} />
                 </form> 
                 <p className={CSS["side-margin"]}>¿No tienes cuenta? <a>Regístrate</a></p>
             </div>
